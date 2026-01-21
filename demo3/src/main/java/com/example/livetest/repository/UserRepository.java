6a92b48084cdcb4c8d36ae3f9a6f935ace7f9993
AdityaPatel-12 <aditya196667@gmail.com>
2026-01-21 19:11:13 +0530

    Delete demo3 directory

--- demo3/src/main/java/com/example/livetest/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.livetest.repository;
-
-import com.example.livetest.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
